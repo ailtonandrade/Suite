c5145a197afad41d2020ee4d14da3cc211309f76
Ailton Andrade de Carvalho <ailtonandrade15@gmail.com>
2024-08-07 21:13:53 -0300

    integrate db

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
 import {fb, db} from './js/firebaseConfiguration.js';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-
+import { sdb } from './js/configuration.js';
 const authFs = getAuth(fb);
-export {getAuth, signInWithEmailAndPassword, authFs, db}
+export {sdb, getAuth, signInWithEmailAndPassword, authFs, db}
